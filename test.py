148ff72822472ca194443fff1d10d3e518a3c19b
Alicja Frankowicz <Alicja Frankowicz>
2018-06-17 00:31:19 +0200

    saving

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from math import *
 import time
 import os
+from tensorflow.python.saved_model import tag_constants
 
 
 def shuffle_dataset(data, results):
@@ -39,26 +40,6 @@ def prepare_data(equation, lim_min, lim_max, step, learn_pg):
     test_results = np.reshape(test_results, (-1, 1))
     return data, results, train_data, train_results, test_data, test_results
 
-
-def init_weights(shape, xavier_params = (None, None)):
-    (fan_in, fan_out) = xavier_params
-    low = -1*np.sqrt(6.0/(fan_in + fan_out)) # {sigmoid:4, tanh:1}
-    high = 1*np.sqrt(6.0/(fan_in + fan_out))
-    return tf.Variable(tf.random_uniform(shape, minval=low, maxval=high, dtype=tf.float32))
-
-def init_biases(shape):
-    return tf.Variable(tf.zeros(shape, dtype=tf.float32))
-
-def model(X, num_hidden=10):
-    w_h = init_weights([1, num_hidden], xavier_params=(1, num_hidden))
-    b_h = init_biases([1, num_hidden])
-    h = tf.nn.tanh(tf.matmul(X, w_h) + b_h)
-
-    w_o = init_weights([num_hidden, 1], xavier_params=(num_hidden, 1))
-    b_o = init_biases([1, 1])
-    return tf.matmul(h, w_o) + b_o
-
-
 if __name__ == '__main__':
     os.system('cls' if os.name == 'nt' else 'clear')
     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
@@ -89,36 +70,15 @@ if __name__ == '__main__':
     learning_rate = 0.01
     training_epochs = 1000
 
-    with tf.Graph().as_default():
-        x = tf.placeholder(dtype=tf.float32, shape=[None, input_size], name="x")
-        y = tf.placeholder(dtype=tf.float32, name="y")
-
-        y_ = model(x, h_size)
-        batch_size = 100
-        optimizer = tf.train.AdamOptimizer().minimize(tf.nn.l2_loss(y_ - y))
-
+    with tf.Graph().as_default() as graph:
         with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            for epoch in range(training_epochs):
-                i = 0
-                for i in range(training_epochs):
-                    start = i
-                    end = i + batch_size
-                    batch_x = np.array(train_data[start:end])
-                    batch_y = np.array(train_results[start:end])
-                    sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-                    i += batch_size
-                mse = sess.run(tf.nn.l2_loss(y_ - train_results), feed_dict={x: train_data})
-                print("Epoch = %d,MSE = %.2f" % (epoch + 1, mse))
-                if mse < 0.01:
-                    break
+            tf.saved_model.loader.load(sess, [tag_constants.SERVING], 'test_model')
+            x = graph.get_tensor_by_name('x:0')
+            y = graph.get_tensor_by_name('y:0')
 
-            inputs = {"x": x, "y": y}
-            outputs = {"prediction": y_}
-            tf.saved_model.simple_save(sess, 'test_model', inputs, outputs)
+            y_ = graph.get_tensor_by_name('add_1:0')
 
-            output = sess.run(y_, feed_dict={x:test_data})
-
+            output = sess.run(y_, feed_dict={x: test_data})
     plt.plot(test_data, test_results, "bo")
     plt.plot(test_data, output, "go")
     plt.show()
